8f6447f99f15fe5f94aa4783c4e6393367d80162
SarahAndDuck <ratkevirina@gmail.com>
2025-01-19 14:13:30 +0700

    fix: add add formatting for 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,34 +1,38 @@
-const { mkdir } = require('node:fs/promises');
 const fs = require('fs');
 const path = require('path');
-
 
 fs.open(path.join(__dirname, 'project-dist', 'bundle.css'), 'w', (err) => {
   if (err) throw err;
 });
 
-fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
-  if (err) {
-    console.log(err);
-  } else {
-    files.forEach((file) => {
-
-      if (file.isFile() && path.extname(file.name) === '.css') {
-        // ===========================================================================
-        fs.readFile(
-          path.join(__dirname, 'styles', file.name), 'utf8',
-          (err, data) => {
-            if (err) throw err;
-
-            fs.appendFile(path.join(__dirname, 'project-dist', 'bundle.css'), String(data), (err) => {
+fs.readdir(
+  path.join(__dirname, 'styles'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) {
+      console.log(err);
+    } else {
+      files.forEach((file) => {
+        if (file.isFile() && path.extname(file.name) === '.css') {
+          // ===========================================================================
+          fs.readFile(
+            path.join(__dirname, 'styles', file.name),
+            'utf8',
+            (err, data) => {
               if (err) throw err;
-            });
-          },
-        );
-        // ===========================================================================
-      }
-    });
 
-  }
-});
-
+              fs.appendFile(
+                path.join(__dirname, 'project-dist', 'bundle.css'),
+                String(data),
+                (err) => {
+                  if (err) throw err;
+                },
+              );
+            },
+          );
+          // ===========================================================================
+        }
+      });
+    }
+  },
+);
